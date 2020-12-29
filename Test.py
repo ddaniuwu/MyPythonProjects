c4dcf4ebf9c5a85c1f3c63e8bae9d5e9e842f7de
ddaniuwu <luis.daniel132@outlook.es>
2020-12-28 23:51:13 -0600

    commitScanner2

---
+++ Test.py
@@ -0,0 +1,13 @@
+import cv2
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+
+image =cv2.imread('Captura3.png')
+
+text=pytesseract.image_to_string(image)
+print('Texto:',text)
+
+cv2.imshow('Image',image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
